6d76f49d38fdbecc2b6db2f36fcf9accd306dac5
MinightDev <onlyayoub565@gmail.com>
2023-06-15 08:54:39 -0700

    A Quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+* main - Entry point
+*
+*
+* Return: (1) Success
+*/
+int main(void)
+{
+	printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stderr);
+	return (1);
+}
