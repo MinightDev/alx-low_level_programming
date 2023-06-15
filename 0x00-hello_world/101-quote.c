628ab945e60845f2cf7262af744ece1f14642ef6
MinightDev <onlyayoub565@gmail.com>
2023-06-15 09:22:45 -0700

    A Quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -9,7 +9,7 @@ int main(void)
 {
 	char quot[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 
-	write(1, quott, sizeof(quot) - 1);
+	write(1, quot, sizeof(quot) - 1);
 
 	return (1);
 }
