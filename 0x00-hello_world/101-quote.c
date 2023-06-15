c1168c1efc478a2780949ff728a7920a35e7149a
MinightDev <onlyayoub565@gmail.com>
2023-06-15 09:20:05 -0700

    A Quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <unistd.h>
 /**
 * main - Entry point
 *
@@ -7,7 +8,7 @@
 */
 int main(void)
 {
-	char quote[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+	char quote[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 
 	write(1, quote, sizeof(quote) - 1);
 
