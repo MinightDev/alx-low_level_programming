d312fda10f9d58681460a8d5b14ea7bb96d355f0
MinightDev <onlyayoub565@gmail.com>
2023-06-16 07:14:50 -0700

    A to Z

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/* more headers goes there */
+
+/**
+ * main - Entry point
+ *
+ * Return: (0) Success
+ */
+int main(void)
+{
+	for (int i = 97; i <= 122; i++)
+	{
+		putchar(i);
+	}
+	/* your code goes there */
+	return (0);
+}
