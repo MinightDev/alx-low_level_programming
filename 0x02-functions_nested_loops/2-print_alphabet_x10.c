060d8d8a9767cea9d2475b3f676f0e34a23f66fd
MinightDev <onlyayoub565@gmail.com>
2023-06-20 07:20:00 -0700

    10x alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - Entry point
+ *
+ * Return: (0) Success
+ */
+
+void print_alphabet_x10(void)
+{
+	int i;
+	int j;
+
+	for (i = 0; i <= 10; i++)
+	{
+		for (j = 97; j <= 122; j++)
+		{
+			_putchar(j);
+		}
+	}
+
+	_putchar('\n');
+}
