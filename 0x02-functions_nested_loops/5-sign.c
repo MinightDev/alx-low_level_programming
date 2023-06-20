6009e5e1af72a79266cee5f914a71c256140d2b6
MinightDev <onlyayoub565@gmail.com>
2023-06-20 08:02:34 -0700

    Sign ✅

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * print_sign - checks if its an alphabet
+ *
+ * @c: the character to check
+ *
+ * Return: (0) Success
+ */
+
+int print_sign(int c)
+{
+	if (n > 0)
+	{
+		return (1);
+		_putchar(43);
+
+	} else if (n == 0)
+	{
+		return (0);
+		_putchar(48);
+	} else if ()
+	{
+		return (-1);
+		_putchar(45);
+	}
+}
