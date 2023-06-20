93f2e77a5d61e555b4d97e62b524d52a527d085c
MinightDev <onlyayoub565@gmail.com>
2023-06-20 14:10:32 -0700

    Times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -7,5 +7,28 @@
 
 void times_table(void)
 {
+	int i, j;
 
+	for (i = 0; i <= 9; i++)
+	{
+		_putchar('0');
+
+		for (j = 1; j <= 9; j++)
+		{
+			int k = i * j;
+			_putchar(',');
+			_putchar(' ');
+
+			if (k <= 9)
+			{
+				_putchar(' ');
+			} else
+			{
+				_putchar((k / 10) + '0');
+			}
+			_putchar((k % 10) + '0');
+		}
+
+		_putchar('\n');
+	}
 }
