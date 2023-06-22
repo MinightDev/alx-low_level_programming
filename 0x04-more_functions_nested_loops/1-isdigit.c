c9a4d9f6781061c6619934346204131a58e9a745
MinightDev <onlyayoub565@gmail.com>
2023-06-22 07:47:42 -0700

    isUpper

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * _isupper - checks if the character is uppercase
+ * @c: The character to check
+ *
+ * Return: On success 1.
+ */
+
+int _isupper(int c)
+{
+	int i;
+
+	for (i = 65; i <= 90; i++)
+	{
+		if (c = _putchar(i))
+		{
+			return (1);
+		} else 
+		{
+			return (0);
+		}
+	}
+}
