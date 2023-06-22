9c3a4bb994be65279acd781fdc44edc495b7f896
MinightDev <onlyayoub565@gmail.com>
2023-06-22 07:51:57 -0700

    isUpper

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -8,16 +8,11 @@
 
 int _isupper(int c)
 {
-	int i;
-
-	for (i = 65; i <= 90; i++)
+	if (c >= 65 && c <= 90)
 	{
-		if (c = _putchar(i))
-		{
-			return (1);
-		} else 
-		{
-			return (0);
-		}
+		return (1);
+	} else 
+	{
+		return (0);
 	}
 }
