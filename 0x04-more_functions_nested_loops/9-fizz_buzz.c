2e60e9eeca115d30f5f01edba3b637693ed6f836
MinightDev <onlyayoub565@gmail.com>
2023-06-22 12:16:46 -0700

    Fizz-Buzz 🐝

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -16,13 +16,13 @@ int main(void)
 			printf("FizzBuzz ");
 		} else if (i % 3 == 0)
 		{
-			printf("Fizz");
+			printf("Fizz ");
 		} else if (i % 5 == 0)
 		{
-			printf("Buzz");
+			printf("Buzz ");
 		} else
 		{
-			printf("%d", i);
+			printf("%d ", i);
 		}
 	}
 
