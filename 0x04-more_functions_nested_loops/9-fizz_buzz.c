91198fac9a466ad98d4d116ae21dcd1d880d675d
MinightDev <onlyayoub565@gmail.com>
2023-06-22 12:15:39 -0700

    Fizz-Buzz 🐝

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: 0 if success
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if (i % 15 == 0)
+		{
+			printf("FizzBuzz ");
+		} else if (i % 3 == 0)
+		{
+			printf("Fizz");
+		} else if (i % 5 == 0)
+		{
+			printf("Buzz");
+		} else
+		{
+			printf("%d", i);
+		}
+	}
+
+	return (0);
+}
