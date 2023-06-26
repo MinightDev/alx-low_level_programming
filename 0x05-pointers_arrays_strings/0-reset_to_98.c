1dbb62b36518276ec6e1c4d97aa2028062598e6e
MinightDev <onlyayoub565@gmail.com>
2023-06-26 04:20:02 -0700

    Pointers arrays n strings 👈

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * reset_to_98 - updates the value it points to to 98.
+ * @n: points to the address of an int
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+	return (*n);
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+Pointers Arrays n strings 👈
