cbefd8e61f2090b5b8f5da29551a387802902491
MinightDev <onlyayoub565@gmail.com>
2023-06-26 07:17:06 -0700

    Reverse string

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * rev_string - reverses a string
+ * @s: the string to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+
+void rev_string(char *s)
+{
+	int i;
+	int len = (_strlen(s) - 1);
+
+	for (i = len; i >= 0; i--)
+	{
+		_putchar(s[i]);
+	}
+	_putchar('\n');
+}
