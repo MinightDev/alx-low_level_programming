5902ef29d54bbc5702da4d965ea1fb4598e50016
MinightDev <onlyayoub565@gmail.com>
2023-06-26 09:46:48 -0700

    Winning is only half of it. Having fun is the other half

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * puts2 - functions that prints every other character of a string
+ * @str: the string to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+
+void puts2(char *str)
+{
+	int i;
+	int n;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+	}
+
+	n = i / 2;
+
+	for (i = n; str[i] != '\0'; i++)
+	{
+		_putchar(str[i]);
+	}
+}
