154637422fe0bb8df4e14f81cbb4b287e7a627f5
MinightDev <onlyayoub565@gmail.com>
2023-06-26 08:51:04 -0700

    Half the lies they about me aren't true

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * puts2 - functions that prints every other character of a string
+ * @str: the string to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+
+void puts2(char *str)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (i % 2 == 0)
+		{
+			_putchar(str[i]);
+
+		}
+	}
+	_putchar('\n');
+}
