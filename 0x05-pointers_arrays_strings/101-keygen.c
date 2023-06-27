3881f3abb451676b8585418d6b5ba30fdb2e183a
MinightDev <onlyayoub565@gmail.com>
2023-06-26 18:29:57 -0700

    Don't hate the hacker, hate the code

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -10,12 +10,18 @@
 int main(void)
 {
 	int i;
-	int size = rand() % (20 + 1 - 30) + 30;
+	int size = rand() % (9 + 1 - 15) + 15;
+
 	srand(time(0));
 
 	for (i = 0; i <= size; i++)
 	{
-		printf("%c", rand() % (125 + 1 - 33) + 33);
+		int random1 = rand() % (57 + 1 - 48) + 48;
+		int random2 = rand() % (47 + 1 - 33) + 33;
+		int random3 = rand() % ('z' - 'a' + 1) + 'a';
+		int random4 = rand() % ('Z' - 'A' + 1) + 'A';
+
+		printf("%c%c%c%c", random1, random2, random3, random4);
 	}
 
 	return (0);
