a10471a265bde305631cbae2d6ffd31fdb302bc9
MinightDev <onlyayoub565@gmail.com>
2023-06-26 17:28:48 -0700

    Don't hate the hacker, hate the code

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - generates random valid password
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int main(void)
+{
+	srand(time(0));
+
+	int i;
+	int size = 20;
+
+	for (i = 0; i <= size; i++)
+	{
+		printf("%c", rand() % (125 + 1 - 33) + 33);
+	}
+}
