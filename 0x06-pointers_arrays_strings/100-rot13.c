8be9caae970c6030003825430a59add8ec788779
MinightDev <onlyayoub565@gmail.com>
2023-07-02 15:36:43 -0700

    rot13

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -1,15 +1,15 @@
 #include "main.h"
 /**
  * rot13 - write a function that encodes a string using rot13
- * @str: targeted string
+ * @s: targeted string
  * Return: a pointer to the resulting string
  * On error, -1 is returned, and errno is set appropriately.
  */
-char *rot13(char *str)
+char *rot13(char *s)
 {
 	int i = 0;
 
-	for (; str[i] != '\0'; i++)
+	for (; s[i] != '\0'; i++)
 	{
 		for (; (s[i] >= 'a' && s[i] <= 'z') || (s[i] >= 'A' && s[i] <= 'Z'); i++)
 		{
@@ -24,5 +24,5 @@ char *rot13(char *str)
 
 		}
 	}
-	return (str);
+	return (s);
 }
