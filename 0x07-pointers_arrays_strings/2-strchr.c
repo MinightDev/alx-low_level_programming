1efe4764e0901d8b474728a20721de2ee29bfdc9
MinightDev <onlyayoub565@gmail.com>
2023-07-03 09:47:41 -0700

    strchr

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -8,13 +8,15 @@
  */
 char *_strchr(char *s, char c)
 {
-	while (*s != '\0')
+	int i;
+
+	for (i = 0; s[i] >= '\0'; i++)
 	{
-		if (*s == c)
+		if (s[i] == c)
 		{
-			return ((char *) (s));
+			return (s + i)
 		}
-		s++;
 	}
-	return (s);
+
+	return ('\0');
 }
