c5583e30cb2465d8f96d6b2ccddd5bf70a31d3b8
MinightDev <onlyayoub565@gmail.com>
2023-07-03 07:25:48 -0700

    main.h

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,4 +1,6 @@
-int _putchar(char c);
+#ifndef FILE_HOLBERTON
+#define FILE_HOLBERTON
+
 char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
 char *_strchr(char *s, char c);
@@ -6,5 +8,8 @@ unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
 void print_chessboard(char (*a)[8]);
+int _putchar(char c);
 void print_diagsums(int *a, int size);
 void set_string(char **s, char *to);
+
+#endif
