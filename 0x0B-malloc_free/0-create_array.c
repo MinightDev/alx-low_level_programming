3e33d7ee328d05205c2404a8fc9a64ae9cc930c0
MinightDev <onlyayoub565@gmail.com>
2023-07-10 06:53:11 -0700

    Float like a butterfly, sting like a bee

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -18,6 +18,11 @@ char *create_array(unsigned int size, char c)
 
 	arr = malloc(sizeof(char) * size);
 
+	if (arr == NULL)
+	{
+		return (NULL);
+	}
+
 	for (i = 0; i < size; i++)
 	{
 		arr[i] = c;
