69e3dabed57e7925ab3e56db2c35c28ab8fd9d4f
MinightDev <onlyayoub565@gmail.com>
2023-07-18 16:03:30 -0700

    newdog

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -5,7 +5,6 @@
  * @name: the dog's name
  * @age: the dog's age
  * @owner: dog's owner
- *
  * Return: pointer to new dog
  */
 dog_t *new_dog(char *name, float age, char *owner)
@@ -25,15 +24,18 @@ dog_t *new_dog(char *name, float age, char *owner)
 	if (dog == NULL)
 		return (NULL);
 	dog->name = malloc(i * sizeof(char));
-
 	if (dog->name == NULL)
+	{
 		free(dog);
 		return (NULL);
+	}
 	dog->owner = malloc(j * sizeof(char));
 	if (dog->owner == NULL)
+	{
 		free(dog->name);
 		free(dog);
 		return (NULL);
+	}
 	while (k < i)
 	{
 		dog->name[k] = name[k];
@@ -41,10 +43,10 @@ dog_t *new_dog(char *name, float age, char *owner)
 	}
 	dog->age = age;
 	k = 0;
-
 	while (k < j)
+	{
 		dog->owner[k] = owner[k];
 		k++;
-
+	}
 	return (dog);
 }
