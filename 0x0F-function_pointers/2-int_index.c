aa35b607d2293dfa80e85e00bd8052529ef3ad40
MinightDev <onlyayoub565@gmail.com>
2023-07-19 07:50:48 -0700

    index

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -10,7 +10,7 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i = 0;
 
-	if (array == NULL || action == NULL || size <= 0)
+	if (array == NULL || cmp == NULL || size <= 0)
 	{
 		return (-1);
 	}
