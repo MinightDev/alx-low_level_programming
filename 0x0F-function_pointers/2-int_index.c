edd0815c77bb03b557f7cc64ad391b66e5caad45
MinightDev <onlyayoub565@gmail.com>
2023-07-19 07:49:18 -0700

    index

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,28 @@
+#include "function_pointers.h"
+/**
+ * int_index - function that executes a function given as a parameter
+ * @array: arr
+ * @size: size
+ * @cmp: pointer to comparaison func
+ * Return: Index of the matching element, else -1 if no match or size <= 0
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i = 0;
+
+	if (array == NULL || action == NULL || size <= 0)
+	{
+		return (-1);
+	}
+	while (i < size)
+	{
+		if (cmp(array[i]) != 0)
+		{
+			return (i);
+		}
+
+		i++;
+	}
+	return (-1);
+}
+
