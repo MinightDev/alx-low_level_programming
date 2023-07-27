da40796d7169cc518275dde2689c9ccba01a882d
MinightDev <onlyayoub565@gmail.com>
2023-07-27 11:35:55 -0700

    printf

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -8,23 +8,16 @@
  */
 size_t print_list(const list_t *h)
 {
-	size_t i;
-
-	i = 0;
+	size_t i = 0;
 
 	while (h)
 	{
 		if (h->str == NULL)
-		{
 			printf("[0] (nil)\n");
-		}
 		else
-		{
 			printf("[%u] %s\n", h->len, h->str);
-		}
-
-		h = h->next;
 		i++;
+		h = h->next;
 	}
 	return (i);
 }
