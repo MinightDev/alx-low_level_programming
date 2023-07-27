ec157ff7564cc7d462c410c60ad2d43fafdb75f5
MinightDev <onlyayoub565@gmail.com>
2023-07-27 10:57:18 -0700

    add node

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,5 +1,41 @@
 #include "lists.h"
+/**
+ * _strdup - function that returns a pointer to a newly alloc space
+ * @str: str
+ * Return: a pointer to the array, or NULL if it fails
+ */
+char *_strdup(const char *str)
+{
+	int i = 0;
+	int j = 0;
+	char *s;
 
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+
+	while (str[i] != '\0')
+	{
+		i++;
+	}
+	s = malloc(sizeof(char) * (i + 1));
+
+	if (s == NULL)
+	{
+		return (NULL);
+	}
+
+	while (j < i)
+	{
+		s[j] = str[j];
+		j++;
+	}
+
+	s[j] = '\0';
+
+	return (s);
+}
 /**
  * add_node - adds a new node
  * @head: head
@@ -23,7 +59,7 @@ list_t *add_node(list_t **head, const char *str)
 	{
 		return (NULL);
 	}
-	new->str = strdup(str);
+	new->str = _strdup(str);
 	new->len = len;
 	new->next = *head;
 	*head = new;
