9e12f10829db0718534a515a126feb688783d291
MinightDev <onlyayoub565@gmail.com>
2023-10-06 07:15:18 -0700

    hash

---
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -0,0 +1,31 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_delete - Deletes a hash table.
+ * @ht: The hash table to delete.
+ */
+void hash_table_delete(hash_table_t *ht)
+{
+	unsigned long int i;
+	hash_node_t *current_node, *temp_node;
+
+	if (ht == NULL)
+		return;
+
+	for (i = 0; i < ht->size; i++)
+	{
+		current_node = ht->array[i];
+		while (current_node != NULL)
+		{
+			temp_node = current_node;
+			current_node = current_node->next;
+			free(temp_node->key);
+			free(temp_node->value);
+			free(temp_node);
+		}
+	}
+
+	free(ht->array);
+	free(ht);
+}
+
