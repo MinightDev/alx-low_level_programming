20b44550565e9c6678aabd56776db43f80bd7c0c
MinightDev <onlyayoub565@gmail.com>
2024-01-17 07:04:05 -0800

    Searching Algos 🔍

--- 0x1E-search_algorithms/103-exponential.c
+++ 0x1E-search_algorithms/103-exponential.c
@@ -2,21 +2,13 @@
 #include <stdlib.h>
 #include "search_algos.h"
 
-/**
- * exponential_search - Searches for a value in a sorted array
- * using Exponential search.
- * @array: Pointer to the first element of the array.
- * @size: Number of elements in the array.
- * @value: Value to search for.
- *
- * Return: The first index where the value is located, or -1 if not found.
- */
 int exponential_search(int *array, size_t size, int value)
 {
+	size_t bound = 1;
+	size_t low, high;
+
 	if (array == NULL)
 		return (-1);
-
-	size_t bound = (1);
 
 	while (bound < size && array[bound] < value)
 	{
@@ -24,8 +16,8 @@ int exponential_search(int *array, size_t size, int value)
 		bound *= 2;
 	}
 
-	size_t low = bound / 2;
-	size_t high = bound < size ? bound : size - 1;
+	low = bound / 2;
+	high = bound < size ? bound : size - 1;
 
 	printf("Value found between indexes [%lu] and [%lu]\n", low, high);
 
